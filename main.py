43a837b5df2b2bdee879d0b265345984c6c33f71
LeanneTN <2321604632@qq.com>
2022-09-02 21:15:56 +0800

    framework of main.py

--- main.py
+++ main.py
@@ -1,6 +1,14 @@
 import torch
-from torchsummary import summary
+import argparse
 
+from torchsummary import summary
+from model.vae import VAE
+
+
+# 调用时传入参数的设置
+parser = argparse.ArgumentParser()
+parser.add_argument('--model', type=str, required=True, help='select a model to train')
+main_args = parser.parse_args()
 
 if torch.cuda.is_available():
     log("cuda available, using gpu")
@@ -8,3 +16,9 @@ device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 dataset_path = 'dataset/csv/original/clean.csv'
 
 # 接下来就是根据不同的参数类型选择不同的数据载入和划分方式
+model_type = main_args.model
+if model_type == 'vae':
+        # todo: 获取VAE传入值的参数
+    # model = VAE()
+    # summary(model=model.float(), (input_dim,))
+

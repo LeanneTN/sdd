5960b98f1cfd3a7870646b7d8685eae6a4097edd
睿啊 <1679430660@qq.com>
2022-09-02 23:29:58 +0800

    add svm and f_measure

---
+++ model/svm.py
@@ -0,0 +1,36 @@
+from sklearn.svm import SVC
+from util.train import precision, recall, f_measure
+
+class SVM:
+
+    def __init__(self):
+        self.my_svm = SVC()
+
+    def train(self, x, y):
+        self.my_svm.fit(x, y)
+
+    def test(self, x, y):
+        y_pred = self.my_svm.predict(x)
+        length = len(y)
+        tp = 0
+        fp = 0
+        tn = 0
+        fn = 0
+        for i in range(length):
+            if y[i] == 0:
+                if y_pred[i] == 0:
+                    tn += 1
+                else:
+                    fp += 1
+            else:
+                if y_pred[i] == 1:
+                    tp += 1
+                else:
+                    fn += 1
+
+        val_precision = precision(tp, fp)
+        val_recall = recall(tp, fn)
+        val_acc = f_measure(val_precision, val_recall)
+
+        print(
+            'val_acc: %f  val_precision: %f  val_recall: %f' % (val_acc, val_precision, val_recall))
--- util/train.py
+++ util/train.py
@@ -1,12 +1,20 @@
+#查准率
 def precision(true_positive, false_positive):
     if true_positive + false_positive == 0:
         return 0.0
     else:
         return true_positive / (true_positive + false_positive)
 
-
+#查全率
 def recall(true_positive, false_negative):
     if true_positive + false_negative == 0:
         return 0.0
     else:
         return true_positive / (true_positive + false_negative)
+
+#查全率和查准率的调和平均数
+def f_measure(precision, recall):
+    if precision + recall == 0:
+        return 0.0
+    else:
+        return (2 * precision * recall) / (precision + recall)

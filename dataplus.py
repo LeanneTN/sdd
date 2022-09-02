8b357a8c3f506d8b8412069608932fed75d9f34d
yi_jin <1837935416@qq.com>
2022-09-02 16:23:18 +0800

    csv_merge

--- dataplus.py
+++ dataplus.py
@@ -1,5 +1,7 @@
 from scipy.io import arff
 import pandas as pd
+#glob用于查找文件目录
+import glob
 
 
 def arff_to_csv(arff_path, csv_path) -> None:
@@ -30,7 +32,28 @@ def arff_to_csv(arff_path, csv_path) -> None:
         csv_data.to_csv(csv_path, index=False)
 
 
-def csv_merge()
+def csv_merge(file_reg: str, merge_path: str) -> None:
+    """
+    csv文件合并
+
+    :param file_reg: csv文件路径的正则匹配
+    :param merge_path: 合并之后的csv文件路径
+    :return: None
+    """
+    data_frame = pd.DataFrame()
+    index = 0
+    for file in glob.glob(file_reg):
+        data = pd.read_csv(file)
+        #对csv文件逐个合并
+        data_frame = pd.concat([data_frame, data], ignore_index=True)
+        #对已经完成合并的文件打印信息
+        print(file + ' finished')
+        print('%d rows, %d columns' % (data.shape[0], data.shape[1]))
+        index += 1
+    #打印总行数
+    print('%d rows total' % data_frame.shape[0])
+    #将DataFrame写入提供路径下的csv文件中，且不需要添加索引
+    data_frame.to_csv(merge_path, index=False)
 
 # if __name__ == '__main__':
 #   arff_to_csv('../dataset/CleanedData/MDP/D1/CM1.arff')
